c10dee0e782d56163ccbc4652b2a43341d269b14
Giovani Aguirre <me@its-g.io>
2020-01-09 22:05:35 -0600

    Completed higher-order.js problems.

--- higher-order.js
+++ higher-order.js
@@ -21,9 +21,7 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 //Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
-
-
+let evenNumbers = mixedNumbers.filter((num) => num % 2 === 0 ? num : null);
 
 ////////// PROBLEM 2 //////////
 
@@ -44,7 +42,7 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(price => price * 1.07);
 
 
 
@@ -63,7 +61,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce((acc, curr) => acc + curr, 0);
 
 
 
@@ -89,7 +87,7 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(monster => monster.CP > 200 ? monster : null);
 
 
 
@@ -103,12 +101,14 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 // Do not edit code above.
 
 /*
-  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
+  Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication).
+  Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
-
-
+let orderTotals = orders.reduce((acc, curr) => {
+  acc.push((curr.price * (curr.tax + 1)).toFixed(2));
+  return acc;
+}, [])
 
 ////////// PROBLEM 6 //////////
 
@@ -123,9 +123,18 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
 // Do not edit the code above.
 
 /*
-  Use a high order method to create to get the sum of bobsTotal.
+  Use a high order method to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+function getSum(purchases) {
+  let total = 0;
+  for (let i = 0; i < purchases.length; i++) {
+    if (purchases[i].owner === "Bob") {
+      total += purchases[i].price;
+    }
+  }
 
+  return total;
+}
 
+let bobsTotal = getSum(purchases);
